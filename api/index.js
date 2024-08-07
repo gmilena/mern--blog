2607bf9357d182e0ad582e2620bbfbda29958b0d
gmilena <milenagenez@mail.com>
2024-08-07 13:30:02 -0300

    crear ruta de registro y ruta de usuario

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO);
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server corriendoo!!");
 });
+
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
